398c9c1542cc06ec1cac57feb39b55f8a65c50ba
David <davidallais2001@gmail.com>
2023-03-29 16:36:43 +0200

    add Quacks& Comments view

--- src/Form/QuackType.php
+++ src/Form/QuackType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\Quack;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -18,7 +19,13 @@ class QuackType extends AbstractType
             ->add('message', TextareaType::class)
             ->add('tags', CollectionType::class, [
                 'entry_type' => TextType::class,
-            ]);
+            ])
+            ->add('picture', FileType::class, [
+        'label' => false,
+        'multiple' => true,
+        'mapped' => false,
+        'required' => false
+    ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
